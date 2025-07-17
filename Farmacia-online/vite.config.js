3df1f535fa261942786b99b6fd56a70791864305
pedrohenri9398 <skilerlives01@gmail.com>
2025-07-17 07:37:41 -0300

    Deleted vite.config.js

--- Farmacia-online/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'Farmacia-online', // A pasta onde estão index.html, style.css, script.js
-  build: {
-    outDir: 'dist' // Onde os arquivos construídos serão colocados
-  }
-});
