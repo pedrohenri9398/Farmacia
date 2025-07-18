719453d1beff0bc3208f8fb40f50657644330cc7
pedrohenri9398 <skilerlives01@gmail.com>
2025-07-18 17:41:28 -0300

    Updated server.js

--- Farmacia-online/server.js
+++ Farmacia-online/server.js
@@ -65,7 +65,6 @@ app.get('/api/products/:id', async (req, res) => {
     }
 });
 
-// Rota para adicionar um novo produto ao Supabase
 app.post('/api/products', async (req, res) => {
     try {
         const { data, error } = await supabase
