e7ee5d0c10bd82bb4e5a5773deddf4f243215187
pedrohenri9398 <skilerlives01@gmail.com>
2025-07-21 10:49:01 -0300

    Updated server.js

--- Farmacia-online/server.js
+++ Farmacia-online/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000; // Define a porta do servidor
 
 // SUAS CHAVES DO SUPABASE AQUI - COPIE E COLE DA SEÇÃO 'API Keys' DO SEU PROJETO SUPABASE
 const SUPABASE_URL = 'SUA_PROJECT_URL_AQUI'; https://vovyubntbeesdwibrdlf.supabase.co
-const SUPABASE_ANON_KEY = 'SUA_ANON_KEY_AQUI'; // Ex: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiY2RlZmdo...';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdnl1Ym50YmVlc2R3aWJyZGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTk5ODcsImV4cCI6MjA2ODE3NTk4N30.j0l9wfwuTaX9bOFq8G3dXd1_y3MqYUjQfzZlouavn9s'; // Ex: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiY2RlZmdo...';
 
 // Cria o cliente Supabase
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
