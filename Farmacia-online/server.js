4024807d61b524f5561a678ac23569b68c7a10fa
pedrohenri9398 <skilerlives01@gmail.com>
2025-07-21 11:05:22 -0300

    Updated server.js

--- Farmacia-online/server.js
+++ Farmacia-online/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // SUAS CHAVES DO SUPABASE AQUI - COPIE E COLE DA SEÇÃO 'API Keys' DO SEU PROJETO SUPABASE
-const SUPABASE_URL = 'SUA_PROJECT_URL_AQUI'; // Ex: 'https://abcdefghijk.supabase.co'
+const SUPABASE_URL = 'https://vovyubntbeesdwibrdlf.supabase.co'; // Ex: 'https://abcdefghijk.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdnl1Ym50YmVlc2R3aWJyZGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTk5ODcsImV4cCI6MjA2ODE3NTk4N30.j0l9wfwuTaX9bOFq8G3dXd1_y3MqYUjQfzZlouavn9s'; // Ex: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiY2RlZmdo...';
 
 // ... (restante do código do server.js que já te passei) ...
